53d4d3b591cf325db8b45c190d9592485b9f9341
Maurizio Catino <mauriziocatino96@gmail.com>
2025-11-12 14:50:04 +0100

    inizio esercitazione

---
+++ main.js
@@ -0,0 +1,75 @@
+// // let name;
+// // console.log(name)
+
+// name= "Angelo"
+// console.log(name)
+
+// let userName = "Angelo";
+
+// const piGreco = 3.14;
+
+// let number = 10;
+
+// console.log( typeof num);
+
+
+// console.log(num.toString());
+
+// let str = "Ciao";
+// let str2 = 'Ciao';
+// let str3 = `Ciao`;
+
+// let age = 34;
+// let name = "Angelo";
+
+// let frase = `Ciao a tutti mi chiamo ${name}, ho ${age} e sono un docente di Aulab`
+
+// console.log(frase);
+
+
+// let frase2 = 'Ciao a tutti, mi chiamo '  + name  + " ho " + age + " anni e sono un docente di Aulab"
+
+// console.log(frase2);
+
+// let a = 5;
+// let b = "ciao5";
+
+// console.log( a * b);
+
+// let bool = true;
+// console.log ( bool)
+// console.log ( typeof bool)
+
+// console.log( 3 > 2 )  
+// // in console sarà true
+
+// console.log( 3> 5 )
+// // In console sarà false
+
+// let num = 10;
+
+// num++
+
+
+let x = 2
+let y = "2"
+let z = 1
+
+console.log( x == y); 
+// -> in questo caso uscirà true perché applica la type coertiion
+
+console.log(x === y);
+// non applica la type coertion e quindi il risultato sarà false
+
+console.log( x != y);
+// diverso, subisce la type coercion quindi è falso
+
+console.log( x !== y)
+// strettamente diverso
+
+console.log(x > z);
+console.log(x < z);
+console.log(x >= z);
+console.log(x <= z);
+
+console.log('io' || 0);
