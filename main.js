8458e05516b31d228937e5753443a66a615905aa
Maurizio Catino <mauriziocatino96@gmail.com>
2025-11-12 15:47:00 +0100

    completati gli esercizi

--- main.js
+++ main.js
@@ -51,25 +51,98 @@
 // num++
 
 
-let x = 2
-let y = "2"
-let z = 1
+// let x = 2
+// let y = "2"
+// let z = 1
 
-console.log( x == y); 
-// -> in questo caso uscirà true perché applica la type coertiion
+// console.log( x == y); 
+// // -> in questo caso uscirà true perché applica la type coertiion
 
-console.log(x === y);
-// non applica la type coertion e quindi il risultato sarà false
+// console.log(x === y);
+// // non applica la type coertion e quindi il risultato sarà false
 
-console.log( x != y);
-// diverso, subisce la type coercion quindi è falso
+// console.log( x != y);
+// // diverso, subisce la type coercion quindi è falso
 
-console.log( x !== y)
-// strettamente diverso
+// console.log( x !== y)
+// // strettamente diverso
 
-console.log(x > z);
-console.log(x < z);
-console.log(x >= z);
-console.log(x <= z);
+// console.log(x > z);
+// console.log(x < z);
+// console.log(x >= z);
+// console.log(x <= z);
 
-console.log('io' || 0);
+// console.log('io' || 0);
+
+// Esercizio 1
+
+let num1 = 3
+let num2 = 5
+
+let frase1 = `Il valore della variabile num1 è ${num1}`
+
+console.log(frase1);
+
+let frase2 = `Il valore della variabile num2 è ${num2}`
+
+console.log(frase2);
+
+// let stringa = "ciao, sono Maurizio"
+let stringa = "ciao, seguo il corso di Aulab"
+
+let frase3 = `Il valore della variabile stringa è ${stringa}`
+
+console.log(frase3);
+
+const PIGRECO = 3.14
+
+let frase4 = `Il valore di PIGRECO è ${PIGRECO}`
+
+console.log(frase4);
+
+// Esercizio 2
+
+let a = 1
+let b = 2
+let c = 3
+let d = 4
+let e = 5
+
+console.log(a + b + c + d + e);
+
+
+
+console.log((a + b + c + d + e)/ 5);
+let somma = 15
+let media = 3
+let fraseoperazioni = `La somma tra i numeri equivale a ${somma} e la media equivale ${3}`
+
+console.log(fraseoperazioni);
+
+// Esercizio 3
+
+let annocorrente = 2025
+let annodinascita = 1996
+let annitotali = 100
+console.log(annocorrente - annodinascita)
+console.log(annitotali - (annocorrente - annodinascita))
+let anni = 29
+let annirimanenti = 71
+let fraseanni = `Hai ${anni} e ti mancano ${annirimanenti} per compierne 100`
+console.log(fraseanni);
+
+// Esercizio 4
+
+let numb1 = 10
+let numb2 = 16
+let somma1 = 26
+let sott = -6
+let molt = 160
+let div = 0.625
+let fraseop = `Con i numeri da te scelti, i risultati delle varie operazioni sono: somma ${somma1}, sottrazione ${sott}, moltiplicazione ${molt}, divisione ${div}`
+console.log(numb1 + numb2);
+console.log(numb1 - numb2);
+console.log(numb1 * numb2);
+console.log(numb1 / numb2);
+
+console.log(fraseop);
